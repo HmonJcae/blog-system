93f42d63b45e1d7c48e31e081e3e2b32ce834dc3
HmonJcae <1770592392@qq.com>
2020-05-05 14:55:56 +0800

    图片处理

---
+++ src/main/java/com/hmo/controller/ImageController.java
@@ -0,0 +1,54 @@
+package com.hmo.controller;
+
+import com.hmo.common.CommonResult;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+@Controller
+@ResponseBody
+@RequestMapping("/image")
+public class ImageController {
+
+    private static String path="D://temp-rainy//";// 图片路径
+
+    @PostMapping(value = "/")
+    public CommonResult fileUpload(@RequestParam(value = "file") MultipartFile file) {
+        if (file.isEmpty()) {
+            System.out.println("文件为空");
+        }
+        String fileName = file.getOriginalFilename();  // 文件名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+        fileName = UUID.randomUUID() + suffixName; // 新文件名
+        File dest = new File(path + fileName);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return CommonResult.success(fileName);
+    }
+
+    @RequestMapping(value = "/{imageName}",method = RequestMethod.GET,produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] ShowImg(@PathVariable("imageName") String imageName) throws IOException{
+        File file = new File(path+imageName);
+        FileInputStream inputStream = new FileInputStream(file);
+        byte[] bytes = new byte[inputStream.available()];
+        inputStream.read(bytes, 0, inputStream.available());
+        inputStream.close();
+        return bytes;
+    }
+}
